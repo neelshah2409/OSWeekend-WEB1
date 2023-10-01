43adf832cd66958ca07a0795cf51d63858d39ff4
Harshil Khamar <harshilkhamar1@gmail.com>
2023-10-01 20:57:14 +0530

    Backend Updated

--- App.js
+++ App.js
@@ -10,12 +10,15 @@ const app = express();
 const PORT = process.env.PORT;
 
 // app.use(cors());
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-  })
-);
+app.use(cors({
+  origin: ['https://osweekend-web.pages.dev']
+}));
+// app.use(
+//   cors({
+//     origin: "http://localhost:3000",
+//     // credentials: true,
+//   })
+// );
 
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(express.json());
